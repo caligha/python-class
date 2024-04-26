e91440bc99cb6f4701e7c07da4dd1c051a108540
caligha <chiamakaaligha11@gmail.com>
2024-04-26 13:09:14 -0500

    practise

--- abc.py
+++ abc.py
@@ -1,2 +1,59 @@
+# To print multiple thing with one print all we need to do is seperate them with the help of comma
+# \n can only be use with string it help put string in the next line 
 print("hello how you doing", type("hello how you doing"))
+
+# string concatenation add string together
+print ("hello" + " how you doing" + " i am doing good")
+
+a = 2
+print(a)
+
+
+#boolean 
+print(10>11)
+print(10>5)
+
+
+# And operators: #double equal to compare
+print(10>5 and 5>99 )
+print(10>5 and 100==100 )
+
+# Or operator:
+print(10>5 or 5>99 )
+
+
+# if (cond.):
+# statements
+a = 10
+
+if (a>0):
+    print("yes a is greater than 0")
+    
+# if & else statement #the else will only be attach to your last if
+# if (cond.): we can have multiple if condition
+#   statement
+# elif(cond.) we can have multiple elif 
+# statement 2
+#else: we can only have one else
+#    statement_3
+    
+var1 = 100
+
+if (var1 > 10):
+    print(var1, "is greater than 10")
+if (var1 >10):
+    print(var1, "is greater than 10")
+else:
+    print(var1, "is smaller than 10")
+    
+
+a = 10
+if (a < 5):
+    print(a, "is greater than 5")
+elif (a < 7):
+    print(a, "is greater than 7")
+elif (a >9):
+    print(a, "is smaller than 9")
+else:
+    print(statement)
 
